327b7810f928ca301987ba404399ff42cfe3d54d
xlo33 <xiaolin.ong.2017@socsc.smu.edu.sg>
2020-08-18 10:31:01 +0800

    nil

--- run_analysis.R
+++ run_analysis.R
@@ -1,40 +1,42 @@
 library(readr)
 library(dplyr)
-activity<-read.table("activity_labels.txt")
-features<-read.table("features.txt")
+
+activity<-read.table("activity_labels.txt", col.names = c("n","functions"))
+features<-read.table("features.txt", col.names = c("code","functions"))
 #TEST data##############################
-x_test<-read.table("test/x_test.txt", col.names = features$V2) 
-x_test$activity <- y_test$V1
-y_test<-read.table("test/y_test.txt")
-subject_test<-read.table("test/subject_test.txt")
-x_test$subject <- factor(subject_test$V1)
+x_test<-read.table("test/x_test.txt", col.names = features$functions) 
+y_test<-read.table("test/y_test.txt", col.names = "code")
+subject_test<-read.table("test/subject_test.txt", col.names = "subject")
 
 #TRAIN data#############################
-x_train<-read.table("train/x_train.txt", col.names = features$V2) #features
-y_train<-read.table("train/y_train.txt") #activity label
-x_train$activity <- y_train$V1
-subject_train<-read.table("train/subject_train.txt")
-x_train$subject <- factor(subject_train$V1)
-
+x_train<-read.table("train/x_train.txt", col.names = features$functions) #features
+y_train<-read.table("train/y_train.txt", col.names = "code") #activity label
+subject_train <- read.table("train/subject_train.txt", col.names = "subject")
 # MERGE DATASET ##########
-df <- rbind(x_test,x_train)
-
+x <- rbind(x_train,x_test)
+y <- rbind(y_train,y_test)
+subject <- rbind(subject_train, subject_test)
+merged <- cbind(subject,y,x)
 #mean & sd ###################
-df_mean<-colMeans(df)
-df_mean
-ncol(df)
-df_sd <- df %>% summarise_if(is.numeric, sd)
-df_sd
-
-#add descriptive values for activity label
-df$activitylabel <-factor(df$activity, labels= c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
-
-#tidy data set with the average of each variable for each activity and each subject.
-
-library(reshape2)
-table(df$activity)
-features.colnames = grep("std\\(\\)|mean\\(\\)", colnames(df), value=TRUE)
-df.melt <-melt(df, id = c('activitylabel', 'subject'), measure.vars = features.colnames)
-df.tidy <- dcast(df.melt, activitylabel + subject ~ variable, mean)
-
-write.table(df.tidy, file = "tidydataset.txt", row.names = FALSE)
+TidyData <- merged %>% select(subject, code, contains("mean"), contains("sd"))
+#descriptive activity names to name the activities in the data set
+TidyData$code <- activity[TidyData$code,2]
+#tidy labels the data set with descriptive variable names.
+names(TidyData)[2] = "activity"
+names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
+names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
+names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData)<-gsub("^t", "Time", names(TidyData))
+names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
+names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("angle", "Angle", names(TidyData))
+names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+# second, independent tidy data set with the average of each variable for each activity and each subject.
+TidyDataset <- TidyData %>%
+        group_by(subject, activity) %>%
+        summarise_all(funs(mean))
+write.table(TidyDataset, "TidyDataset.txt", row.name=FALSE)
